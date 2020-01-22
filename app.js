5332c259338a4e036e925aaf0dca1d0b1b128a64
ba061600 <sirisha.garjala@stud.uni-bamberg.de>
2020-01-22 12:57:57 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const taskInput = document.querySelector('#task');
+const form  = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const filter = document.querySelector('#filter');
+const clearBtn = document.querySelector('.clear-tasks');
+
